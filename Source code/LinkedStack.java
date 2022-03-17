667c09268636d7da104121fc64e37fc9ba4662f9
Jonathan Pena <98781952+JonathanPena17@users.noreply.github.com>
2022-03-17 10:36:43 -0700

    Update LinkedStack.java

--- Source code/LinkedStack.java
+++ Source code/LinkedStack.java
@@ -81,53 +81,5 @@ public class LinkedStack<T> implements StackInterface<T>
             next = nextNode;
         }//end setNextNode
 
-    } //end node
-    
-    public static convertToPostfix(infix)
-    {
-    // Converts an infix expression to an equivalent postfix expression.
-    operatorStack = a new empty stack
-    postfix = a new empty string
-       while (infix has characters left to parse)
-       {
-            nextCharacter = next nonblank character of infix 
-            switch (nextCharacter)
-            {
-                case variable: 
-                   Append nextCharacter to postfix
-                   break
-                case '^' : 
-                   operatorStack.push(nextCharacter)
-                   break
-                case '+' : case '-' : case '*' : case '/' : 
-                while (!operatorStack.isEmpty() and
-                    precedence of nextCharacter <= precedence of operatorStack.peek())
-                {
-                   Append operatorStack.peek() to postfix 
-                   operatorStack.pop()
-                } 
-                operatorStack.push(nextCharacter) 
-                break
-             case '( ' : 
-                operatorStack.push(nextCharacter) 
-                break
-             case ')' : // Stack is not empty if infix expression is valid
-                topOperator = operatorStack.pop()
-                while (topOperator != '(')
-                {
-                    Append topOperator to postfix
-                    topOperator = operatorStack.pop()
-                }
-                break
-                default: break // Ignore unexpected characters
-           }
-      }
-      while (!operatorStack.isEmpty()) 
-      {
-            topOperator = operatorStack.pop() 
-            Append topOperator to postfix 
-      }
-      return postfix;
-    }
-    
+    } //end node  
 }//end LinkedStack
